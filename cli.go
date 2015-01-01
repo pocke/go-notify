6e42bbcadcf65e982a2ff3f682b5625d72d3eece
pocke <p.ck.t22@gmail.com>
2015-01-01 21:02:08 +0900

    show version

---
+++ cli.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	ExitCodeOK = iota
+	ExitCodeParseFlagError
+)
+
+type CLI struct {
+	InStream             io.Reader
+	OutStream, ErrStream io.Writer
+
+	Options *Options
+}
+
+type Options struct {
+	Version bool
+}
+
+func NewCLI() *CLI {
+	return &CLI{
+		InStream:  os.Stdin,
+		OutStream: os.Stdout,
+		ErrStream: os.Stderr,
+		Options:   &Options{},
+	}
+}
+
+func (c *CLI) Run(args []string) int {
+	err := c.ParseOption(args)
+	if err != nil {
+		return ExitCodeParseFlagError
+	}
+
+	if c.Options.Version {
+		fmt.Fprintf(c.ErrStream, "%s version %s\n", args[0], Version)
+	}
+
+	return ExitCodeOK
+}
+
+func (c *CLI) ParseOption(args []string) error {
+	f := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	f.SetOutput(c.ErrStream)
+	f.BoolVar(&c.Options.Version, "version", false, "Print version information and quit")
+
+	return f.Parse(args[1:])
+}
---
+++ cli_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestNewCLI(t *testing.T) {
+	cli := NewCLI()
+	if cli.InStream != os.Stdin {
+		t.Errorf("cli.InStream should be Stdin. But got %s", cli.InStream)
+	}
+
+	if cli.OutStream != os.Stdout {
+		t.Errorf("cli.OutStream should be Stdout. But got %s", cli.OutStream)
+	}
+
+	if cli.ErrStream != os.Stderr {
+		t.Errorf("cli.ErrStream should be Stdin. But got %s", cli.ErrStream)
+	}
+
+	if cli.Options == nil {
+		t.Errorf("cli.Options should not be nil. But got nil")
+	}
+}
+
+func TestRun(t *testing.T) {
+	c, _, _, _ := newCLIForTest()
+	code := c.Run([]string{"go-notify"})
+	if code != ExitCodeOK {
+		t.Errorf("Expected %d, But got %d", ExitCodeOK, code)
+	}
+}
+
+func TestRunShowVersion(t *testing.T) {
+	c, _, _, er := newCLIForTest()
+	code := c.Run([]string{"go-notify", "--version"})
+	if code != ExitCodeOK {
+		t.Errorf("Expected %d, But got %d", ExitCodeOK, code)
+	}
+	if !strings.Contains(er.String(), Version) {
+		t.Errorf("Output should include %s, but not.", Version)
+		t.Errorf("Output: %q", er.String())
+	}
+}
+
+func TestParseOption(t *testing.T) {
+	c, _, _, _ := newCLIForTest()
+	c.ParseOption([]string{"go-notify", "--version"})
+
+	if !c.Options.Version {
+		t.Errorf("When --version option, Options.Version should be true. But got false")
+	}
+}
+
+func newCLIForTest() (c *CLI, in, out, err *bytes.Buffer) {
+	in = new(bytes.Buffer)
+	out = new(bytes.Buffer)
+	err = new(bytes.Buffer)
+
+	c = &CLI{
+		InStream:  in,
+		OutStream: out,
+		ErrStream: err,
+		Options:   &Options{},
+	}
+
+	return
+}
